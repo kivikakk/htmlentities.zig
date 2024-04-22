cc17067450d5e0a8693e5199f150ba0a994af047
Amelia Cuss <amelia@kivikakk.ee>
2024-04-22 13:58:42 +0300

    flake.nix: use zig 0.12.0 from zig-overlay.

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  inputs = {
+    zig = {
+      url = github:mitchellh/zig-overlay;
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    zig,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      inherit (pkgs) lib;
+    in rec {
+      packages.default = pkgs.stdenv.mkDerivation {
+        name = "htmlentities.zig-build";
+
+        src = ./.;
+
+        nativeBuildInputs = [
+          zig.packages.${system}."0.12.0"
+        ];
+
+        buildPhase = ''
+          export ZIG_GLOBAL_CACHE_DIR="$TMPDIR/zig"
+          zig build test
+          touch $out
+        '';
+
+        dontInstall = true;
+      };
+
+      formatter = pkgs.alejandra;
+    });
+}
